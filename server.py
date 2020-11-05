effb499b81b342253e99ae7dffdee0c386e252c4
Carlos Rivera <carlos@synx.co>
2020-11-05 01:30:39 -0600

    updated travis

--- server.py
+++ server.py
@@ -1,11 +1,31 @@
+import sys
+import getopt
 import http.server
 import ssl
 
-server_address = ('0.0.0.0', 443)
-httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               server_side=True,
-                               certfile='/etc/letsencrypt/live/niko.kf.synx.io/fullchain.pem',
-                               keyfile='/etc/letsencrypt/live/niko.kf.synx.io/privkey.pem',
-                               ssl_version=ssl.PROTOCOL_TLS)
-httpd.serve_forever()
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "p:", ["port="])
+
+        for opt, arg in opts:
+            if opt in ("-p", "--port"):
+                port = int(arg)
+
+    except getopt.GetoptError:
+        port = 443
+
+    server_address = ('0.0.0.0', port)
+    httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+
+    # assume production
+    if port == 443:
+        httpd.socket = ssl.wrap_socket(httpd.socket,
+                                       server_side=True,
+                                       certfile='/etc/letsencrypt/live/niko.kf.synx.io/fullchain.pem',
+                                       keyfile='/etc/letsencrypt/live/niko.kf.synx.io/privkey.pem',
+                                       ssl_version=ssl.PROTOCOL_TLS)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@ import requests
 
 def test_request_response():
     # Send a request to the server and store the response.
-    response = requests.get('https://localhost/')
+    response = requests.get('https://localhost:8843/')
 
     # Confirm that the request-response cycle completed successfully.
     assert_true(response.ok)
